0b508d553af342d8a821967b6bb9ed13dc833570
浪漫罗比 <huzhifeiatnj@163.com>
2020-06-01 13:53:59 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -6,5 +6,7 @@ def PrintMes(value,color){
               'blue'  : "\033[47;34m ${value} \033[0m",
               'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
               'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+    ansiColor('xterm') {
         println(colors[color])
+    }
 }
